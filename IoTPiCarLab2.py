3b5e0e9afb8645effe3616a8b681168b6adb4f49
sarith sanal <sarithisbeyondu@gmail.com>
2022-02-04 01:02:03 -0600

    Added camera object avoidance for stop sign and person

--- IoTPiCarLab2.py
+++ IoTPiCarLab2.py
@@ -19,6 +19,7 @@ MAP_SIZE = 200
 
 servo = Servo(PWM("P0"), offset=0)
 ultrasonic = Ultrasonic(Pin('D8'), Pin('D9'))
+
 FORWARD_SPEED = 5
 BACKWARD_SPEED = 10
 TURN_SPEED = 30
@@ -32,7 +33,12 @@ SERVO_ZERO_ANGLE = 0
 SERVO_MIN_ANGLE = -90
 SERVO_TIME = 0.5
 servo_currentAngle = SERVO_ZERO_ANGLE
-envoriment_map = np.zeros((MAP_SIZE,MAP_SIZE), dtype='uint8')
+envoriment_map_ultrasonic = np.zeros((MAP_SIZE,MAP_SIZE), dtype=np.uint8)
+envoriment_map_camera = np.empty((MAP_SIZE,MAP_SIZE), dtype="S10")
+CAMERA_OBJECT_NO_OBJECT = 0
+CAMERA_OBJECT_STOP_SIGN = 1
+CAMERA_OBJECT_PERSON = 2
+CAMERA_OBJECT_UNIDENTIFIED = 3
 
 #Considering the car starts from the bottom center
 car_start_map_x = MAP_SIZE/2 -1
@@ -127,7 +133,7 @@ def decide_movement(sweep_info, Isdetected):
 #             print('moving forward')
 #             #move(safe_forward_cycles, movement_type='linear', direction='forward')
 
-def create_advanced_mapping(sweep_info):
+def create_advanced_mapping(sweep_info, cam_result):
     global envoriment_map
     for entry in sweep_info:
         if(entry['detection']):
@@ -135,15 +141,18 @@ def create_advanced_mapping(sweep_info):
             thetaInRadians = math.radians(entry['angle'])
             scanned_object_x = entry['distance']*math.sin(thetaInRadians)
             scanned_object_y = entry['distance']*math.cos(thetaInRadians)
-                        
+            
             #(x,y) with respect to the ma           
             map_x = car_start_map_x + scanned_object_x
             map_y = car_start_map_y + scanned_object_y
             
             if((map_x >= MAP_SIZE) or (map_y >= MAP_SIZE)):
                 print('x:',scanned_object_x, 'y:',scanned_object_y,'xx:',map_x,'yy:',map_y)
-            else:
-                envoriment_map[int(map_x), int(map_y)] = 1
+            elif(not cam_result):
+                envoriment_map_ultrasonic[int(map_x), int(map_y)] = 1
+                VisualizeData(int(map_x), int(map_y), cl='black')
+            else:    
+                envoriment_map_camera[int(map_x), int(map_y)] = cam_result
                 VisualizeData(int(map_x), int(map_y), cl='black')
         
 if __name__ == '__main__':
@@ -174,24 +183,25 @@ if __name__ == '__main__':
         while (cv.waitKey(1) != 27):           
             #get ultrasonic sweep data
             (one_sweep_info, IsDetected) = perform_one_sweep(DETECTION_DISTANCE, servo_speed=0.02)
-            create_advanced_mapping(one_sweep_info)
             #get detected camera objects
             if(camera.isOpened()):
                 success, image = camera.read()
                 if(success):
                     image = cv.flip(image, 1)
                     detections_info = detector.detect(image)
-#                     result = []
-#                     for detected_obect in detections_info:
-#                         label = detected_obect.categories[0].label
-#                         score = detected_obect.categories[0].score
-#                         result.append([label, score])
-#                     print('Object list:',result)
+                    cam_result = ""
+                    for detected_obect in detections_info:
+                        if((detected_obect.categories[0].label == 'person') or
+                           (detected_obect.categories[0].label == 'stop sign')):
+                            cam_result=detected_obect.categories[0].label
+                            break
+                    print('Object list:',cam_result)
                 else:
                     print('Camera read error')                    
             else:
                 print('Camera open error')
-                
+            create_advanced_mapping(one_sweep_info, cam_result)
+            
             decide_movement(one_sweep_info, IsDetected)
         #print(envoriment_map)
 
